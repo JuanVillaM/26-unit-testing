8abab62668b57366980d8d194279ea077d1f8b35
Juan Villa <juanestebanvillamemdoza@gmail.com>
2021-01-08 20:09:33 -0400

    FirstApp.js added

---
+++ src/FirstApp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FirstApp = ({greet, subtitle}) => {
+    return (
+        <>
+            <h1>{ greet }</h1>
+            <p>{ subtitle }</p>
+        </>
+    )
+}
+
+FirstApp.propTypes = {
+    greet: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+    subtitle: 'I am a subtitle.'
+}
+
+export default FirstApp;
